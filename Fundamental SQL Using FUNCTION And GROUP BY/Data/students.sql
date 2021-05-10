e10cb85dc1fe804dfbe0020d44189949ef4e1729
Reza Dilla Saputri <67749434+rezadilla@users.noreply.github.com>
2021-05-10 11:32:00 +0700

    Update students.sql

--- Fundamental SQL Using FUNCTION And GROUP BY/Data/students.sql
+++ Fundamental SQL Using FUNCTION And GROUP BY/Data/students.sql
@@ -16,7 +16,7 @@ INSERT INTO
 	students
 VALUES
 	(1,'Jose','Mohit','Jose_Mohit@gmail.com',64.55,72.6,-8.05),
-    (2,'Lala','Karlina','lala_karlina@yahoo.com',72.85,65.35,7.5),
-    (3,'Sultan','Hadi','Sultan_Hadi@gmail.com',45.32,50.25,-4.93),
-    (4,'Jaya','Usman','jaya_usman@yahoo.com',86.73,77.4,9.33),
-    (5,'Anjali','Wijaya','anjali_wijaya@yahoo.com',92.25,90.75,1.5);
+        (2,'Lala','Karlina','lala_karlina@yahoo.com',72.85,65.35,7.5),
+        (3,'Sultan','Hadi','Sultan_Hadi@gmail.com',45.32,50.25,-4.93),
+        (4,'Jaya','Usman','jaya_usman@yahoo.com',86.73,77.4,9.33),
+        (5,'Anjali','Wijaya','anjali_wijaya@yahoo.com',92.25,90.75,1.5);
